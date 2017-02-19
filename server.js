30f663b030aebd4773108331fade525cc09025ff
Varaprasad-Kurra <prasadkurra31@gmail.com>
2017-02-19 03:17:13 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.sendFile('Article one is selected and Server has handled it');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
