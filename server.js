501da64333343def721a678207ffaf4b00372df3
Varaprasad-Kurra <prasadkurra31@gmail.com>
2017-02-20 09:36:32 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,29 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+   var content=
+         {
+         title:'Article One - by Varam',
+         Heading:'Article one',
+         content:`
+                   <p>
+                "That so many of us are here today is a recognition that the threat from climate change is serious, it is urgent, and it is growing. Our generation's response to this challenge will be judged by history, for if we fail to meet it - boldly, swiftly, and together - we risk consigning future generations to an irreversible catastrophe."
+
+                 These are the words President Obama used to begin his global warming speech before the United Nations Summit in 2009.
+
+                 Search Google for global warming and you will get almost 65 million pages of results.
+         
+                  The subject has certainly drawn a lot of attention. But just what is global warming? What is causing it? What effects does it have on the earth and its inhabitants? What are some possible solutions? These are the questions that I will be addressing in this short, informative speech today.
+
+                   What is Global Warming?
+
+                 - See more at: http://www.best-speech-topics.com/global-warming-speech.html#sthash.BshW9YJb.dpuf 
+        </p>  
+                  
+         };
 
 app.get('/article-one',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui','article-one.html'));
